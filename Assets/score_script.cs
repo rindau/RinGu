d8fc6e35b3405a08e231cadd17419b52dbf4cddd
rindau <48348133+rindau@users.noreply.github.com>
2019-03-22 10:32:20 +0200

    add score
    pridėkome scor'ą

---
+++ Assets/score_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score_script : MonoBehaviour {
+    public int count;
+    public Text sctext;  
+        // Use this for initialization
+	void Start ()
+    {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        DisplayCount();
+	}
+
+    public void DisplayCount()
+    {
+        sctext.text = "SCORE: " + count.ToString();
+    }
+}
